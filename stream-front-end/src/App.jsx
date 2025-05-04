9159b963819bb4f53b8a5eff7193b1006cab4257
HarmanSet <harmansethi774@gmail.com>
2025-05-04 16:42:57 +0530

    Java Project

--- stream-front-end/src/App.jsx
+++ stream-front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Navbar } from 'flowbite-react';
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
 import { Button } from 'flowbite-react';
 import VideoLibrary from './components/VideoLibrary';
 import VideoUpload from './components/VideoUpload';
@@ -8,44 +8,44 @@ import AudioLibrary from './components/AudioLibrary';
 import DocumentLibrary from './components/DocumentLibrary';
 import WelcomePage from './components/WelcomePage'; // Import the WelcomePage
 
-function App() {
+export default function App() {
   return (
     <Router>
       <div className="App">
         <Navbar fluid rounded className="shadow-md bg-white">
-          <Navbar.Brand>
+          <NavbarBrand>
             <Link to="/" className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
               Media Library
             </Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Navbar.Link>
+          </NavbarBrand>
+          <NavbarToggle />
+          <NavbarCollapse>
+            <NavbarLink>
               <Link to="/" className={window.location.pathname === "/" ? "text-blue-700" : ""}>
                 Home
               </Link>
-            </Navbar.Link>
-            <Navbar.Link>
+            </NavbarLink>
+            <NavbarLink>
               <Link to="/videos" className={window.location.pathname === "/videos" ? "text-blue-700" : ""}>
                 Videos
               </Link>
-            </Navbar.Link>
-            <Navbar.Link>
+            </NavbarLink>
+            <NavbarLink>
               <Link to="/audio" className={window.location.pathname === "/audio" ? "text-blue-700" : ""}>
                 Audio
               </Link>
-            </Navbar.Link>
-            <Navbar.Link>
+            </NavbarLink>
+            <NavbarLink>
               <Link to="/documents" className={window.location.pathname === "/documents" ? "text-blue-700" : ""}>
                 Documents
               </Link>
-            </Navbar.Link>
-            <Navbar.Link>
+            </NavbarLink>
+            <NavbarLink>
               <Link to="/upload" className={window.location.pathname === "/upload" ? "text-blue-700" : ""}>
                 Upload
               </Link>
-            </Navbar.Link>
-          </Navbar.Collapse>
+            </NavbarLink>
+          </NavbarCollapse>
         </Navbar>
 
         <main className="bg-gray-50 min-h-screen">
@@ -61,5 +61,3 @@ function App() {
     </Router>
   );
 }
-
-export default App;
